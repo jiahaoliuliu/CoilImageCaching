c9d765bf8f9e6c4fe661d624cda68826c3c06f67
Jiahao Liu <jiahao.liu@glovoApp.com>
2026-01-29 15:45:46 +0100

    Set the button to clear the cache

--- app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import coil3.imageLoader
 import com.jiahaoliuliu.coilimagecaching.ui.theme.CoilImageCachingTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,13 +29,30 @@ class MainActivity : ComponentActivity() {
         val imageUrl = "https://www.livehome3d.com/assets/img/articles/design-house/how-to-design-a-house@2x.jpg"
         setContent {
             CoilImageCachingTheme {
-                AsyncImage(
-                    model = imageUrl,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(1400f/800f)
-                )
+                Scaffold { contentPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(contentPadding)
+                    ) {
+                        AsyncImage(
+                            model = imageUrl,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1400f/800f)
+                        )
+                        Spacer(Modifier.height(16.dp))
+                        Button(
+                            onClick = {
+                                imageLoader.diskCache?.clear()
+                                imageLoader.memoryCache?.clear()
+                            }
+                        ) {
+                            Text("Clear cache")
+                        }
+                    }
+                }
             }
         }
     }
