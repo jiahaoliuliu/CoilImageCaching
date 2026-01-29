919bd643ce28733df31a905eee82ebe343aedefa
Jiahao Liu <jiahao.liu@glovoApp.com>
2026-01-29 15:34:18 +0100

    Set the disk cache and the logger

--- app/src/main/java/com/jiahaoliuliu/coilimagecaching/MyApplication.kt
+++ app/src/main/java/com/jiahaoliuliu/coilimagecaching/MyApplication.kt
@@ -4,8 +4,11 @@ import android.app.Application
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
 import coil3.memory.MemoryCache
 import coil3.request.CachePolicy
+import coil3.util.DebugLogger
 
 class MyApplication: Application(), SingletonImageLoader.Factory {
     override fun newImageLoader(context: PlatformContext): ImageLoader {
@@ -17,6 +20,14 @@ class MyApplication: Application(), SingletonImageLoader.Factory {
                     .strongReferencesEnabled(true)
                     .build()
             }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
             .build()
     }
 }
