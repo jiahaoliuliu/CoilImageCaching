59d3753ce218343b0df3280bad25741f7f0297d8
Jiahao Liu <jiahao.liu@glovoApp.com>
2026-02-06 11:12:01 +0100

    Set three ways to have the place holder

--- app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jiahaoliuliu.coilimagecaching
 
+import android.R.attr.contentDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,13 +15,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.imageLoader
-import coil3.memory.MemoryCache
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import coil3.request.error
+import coil3.request.placeholder
 import com.jiahaoliuliu.coilimagecaching.ui.theme.CoilImageCachingTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,11 +41,41 @@ class MainActivity : ComponentActivity() {
                             .padding(contentPadding)
                     ) {
                         AsyncImage(
+                            model = ImageRequest
+                                .Builder(LocalContext.current)
+                                .data(imageUrl)
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1400f/800f),
+                            placeholder = painterResource(R.drawable.coillogo),
+                            error = painterResource(R.drawable.coillogo)
+                        )
+                        Spacer(Modifier.height(16.dp))
+                        AsyncImage(
+                            model = ImageRequest
+                                .Builder(LocalContext.current)
+                                .data(imageUrl)
+                                .crossfade(true)
+                                .placeholder(R.drawable.coillogo)
+                                .error(R.drawable.coillogo)
+                                .build(),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1400f/800f),
+                        )
+                        Spacer(Modifier.height(16.dp))
+                        AsyncImage(
                             model = imageUrl,
                             contentDescription = null,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .aspectRatio(1400f/800f)
+                                .aspectRatio(1400f/800f),
+                            placeholder = painterResource(R.drawable.coillogo),
+                            error = painterResource(R.drawable.coillogo)
                         )
                         Spacer(Modifier.height(16.dp))
                         Button(
