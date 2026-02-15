2e82e4176fd9ec51be6523671bcc73a260809527
Jiahao Liu <jiahao.liu@glovoApp.com>
2026-02-15 12:46:05 +0100

    Restrict the size for the image

--- app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/coilimagecaching/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.jiahaoliuliu.coilimagecaching
 
-import android.R.attr.contentDescription
 import android.os.Bundle
 import android.os.Debug
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,6 +18,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -26,8 +26,6 @@ import coil3.compose.AsyncImage
 import coil3.imageLoader
 import coil3.request.ImageRequest
 import coil3.request.crossfade
-import coil3.request.error
-import coil3.request.placeholder
 import com.jiahaoliuliu.coilimagecaching.ui.theme.CoilImageCachingTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,46 +38,26 @@ class MainActivity : ComponentActivity() {
                 Scaffold { contentPadding ->
                     Column(
                         modifier = Modifier
-                            .fillMaxSize()
                             .padding(contentPadding)
+                            .fillMaxSize()
                     ) {
-                        AsyncImage(
-                            model = ImageRequest
-                                .Builder(LocalContext.current)
-                                .data(imageUrl)
-                                .crossfade(true)
-                                .build(),
-                            contentDescription = null,
+                        Box (
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .aspectRatio(1400f/800f),
-                            placeholder = painterResource(R.drawable.coillogo),
-                            error = painterResource(R.drawable.coillogo)
-                        )
-                        Spacer(Modifier.height(16.dp))
-                        AsyncImage(
-                            model = ImageRequest
-                                .Builder(LocalContext.current)
-                                .data(imageUrl)
-                                .crossfade(true)
-                                .placeholder(R.drawable.coillogo)
-                                .error(R.drawable.coillogo)
-                                .build(),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1400f/800f),
-                        )
-                        Spacer(Modifier.height(16.dp))
-                        AsyncImage(
-                            model = imageUrl,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .aspectRatio(1400f/800f),
-                            placeholder = painterResource(R.drawable.coillogo),
-                            error = painterResource(R.drawable.coillogo)
-                        )
+                                .height(128.dp)
+                        ) {
+                            AsyncImage(
+                                model = ImageRequest
+                                    .Builder(LocalContext.current)
+                                    .data(imageUrl)
+                                    .crossfade(true)
+                                    .build(),
+                                contentDescription = null,
+                                placeholder = painterResource(R.drawable.house_cropped),
+                                error = painterResource(R.drawable.house_cropped),
+                                contentScale = ContentScale.Crop,
+                            )
+                        }
                         Spacer(Modifier.height(16.dp))
                         Row(
                             modifier = Modifier.fillMaxWidth(),
